1e9b0a2b603c2a2ba0882c53928ddf16a2abe5cd
chaudhuree <chaudhuree00@gmail.com>
2022-02-13 11:12:13 +0600

    list iteration

---
+++ Day 08/listAdvanced00.py
@@ -0,0 +1,18 @@
+# nested list
+nList = [
+    ['sohan', 'chaudhuree'],
+    ['kazi', 'sabrina'],
+    ['sadia', 'shimla'],
+]
+# access nested lists
+print(nList[0][0])
+print(nList[1][1])
+
+# iterate nested lists
+
+for i in nList:
+    for j in i:
+        print(j)
+
+for i in nList:
+  print(i[0])
