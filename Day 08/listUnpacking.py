bf8916b3e3f88b74be0c4b40ca9ee174dbc8756e
chaudhuree <chaudhuree00@gmail.com>
2022-02-14 11:12:13 +0600

    list unpacking

---
+++ Day 08/listUnpacking.py
@@ -0,0 +1,12 @@
+# list unpacking
+details=['shahriar','kabir','sOhan',27]
+firstname,*lastName,age=details
+print(firstname)
+print(lastName)
+print(age)
+
+color=[266,17,154]
+red,green,blue=color
+print(red)
+print(green)
+print(blue)
