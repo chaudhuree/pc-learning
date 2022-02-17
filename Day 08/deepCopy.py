1eaca5075d73bf0db09c882053fbb6429192bbb5
chaudhuree <chaudhuree00@gmail.com>
2022-02-17 11:12:13 +0600

    list copy and deep copy nested list

---
+++ Day 08/deepCopy.py
@@ -0,0 +1,4 @@
+import copy
+list1=[1,2,4,['a','v'],6,7,2]
+list2=copy.deepcopy(list1)
+print(list2)
---
+++ Day 08/listCopy.py
@@ -0,0 +1,7 @@
+list1=[1,2,4,5,7]
+# copy list 
+list2=list1.copy()
+print(list2)
+# another way to copy list
+list3=list1[:]
+print(list3)
