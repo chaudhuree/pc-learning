20b2b6b8db0b6ff48ea76a313a08a94b87089178
chaudhuree <chaudhuree00@gmail.com>
2022-05-15 13:57:09 +0600

    docs: string one

---
+++ Day 02/string.py
@@ -0,0 +1,12 @@
+v1= 'hi there'
+v2= "hi there"
+v3= "i said \"hi there\""
+v4= '''i said "hi there"'''
+v5= "i said i can't do that"
+v6='i said "hi there'
+print(v1)
+print(v2)
+print(v3)
+print(v4)
+print(v5)
+print(v6)
