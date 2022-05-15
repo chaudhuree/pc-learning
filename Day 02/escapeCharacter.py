00c4f15924e3274795e1a429782376feed26ba94
chaudhuree <chaudhuree00@gmail.com>
2022-05-15 16:01:22 +0600

    docs: string escape character and multiline string

---
+++ Day 02/escapeCharacter.py
@@ -0,0 +1,14 @@
+var = "sOhan chaudhuree"
+print(var)
+var1 = "sohan \n chaudhuree"
+print(var1)
+var2 = "sohan \t chaudhuree"
+print(var2)
+
+
+# multi line string
+var3 = """lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(var3)
