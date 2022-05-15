34bbf6a7f820f2e2b8c42f6d27ffc8207b6939e4
chaudhuree <chaudhuree00@gmail.com>
2022-05-15 17:02:19 +0600

    docs:str replace

--- Day 02/stringMethods.py
+++ Day 02/stringMethods.py
@@ -5,8 +5,13 @@ str.lower()
 str.capitalize()
 
 # strip
-str1='   hello   '.strip()
-str2='---hello---'.strip('-')
+str1 = '   hello   '.strip()
+str2 = '---hello---'.strip('-')
 '''remove things from front and back'''
 
+# string replace
+str3 = '1km 2km 3km 4km'
+str3.replace('km', 'm')
 
+str4 = '1km 2km 3km 4km'
+str4.replace('km', 'm', 2)
