1b6a0b7488da4fa535c78ab59fe46cbc384a0715
chaudhuree <chaudhuree00@gmail.com>
2022-05-15 16:57:58 +0600

    docs: strip

---
+++ Day 02/stringMethods.py
@@ -0,0 +1,12 @@
+str = 'sohan chaudhuree'
+# string methods
+str.upper()
+str.lower()
+str.capitalize()
+
+# strip
+str1='   hello   '.strip()
+str2='---hello---'.strip('-')
+'''remove things from front and back'''
+
+
