ab42fc1139ada21e166aad0aa1eb3e3584336f0a
chaudhuree <chaudhuree00@gmail.com>
2022-05-15 15:53:52 +0600

    docs: string slice

---
+++ Day 02/stringSlice.py
@@ -0,0 +1,16 @@
+var = 'hi there how are you'
+var1 = var[0:5]
+var2 = var[6:]
+var3 = var[:6]
+var4 = var[-1:]
+var5 = var[1:8:2]
+var6 = var[::-1]
+var7 = var[0:]
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+print(var7)
+
