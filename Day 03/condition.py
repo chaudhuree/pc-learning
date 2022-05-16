5029b61841c7b7ccdc385e0ac989f03b10c55a18
chaudhuree <chaudhuree00@gmail.com>
2022-05-16 23:41:10 +0600

    docs: if elif and input print with variable and string

--- Day 03/condition.py
+++ Day 03/condition.py
@@ -1,10 +1,36 @@
 # in
-var='a' in 'abba'
+var = 'a' in 'abba'
 print(var)
 var1 = 'A' in 'abba'
 print(var1)
 # 'int' is not iterable
-# var3= 1 in 312
-# print(var3)
-var4= 1 in [2,1,3,4,5,6,7,8,9]
-print(var4)
+# var2= 1 in 312
+# print(var2)
+var3 = 1 in [2, 1, 3, 4, 5, 6, 7, 8, 9]
+print(var3)
+
+# conditions input
+# var = int(input('enter your age'))
+var = 17
+if (var > 18):
+    print('you are in')
+elif (var >= 16):
+    print('come back after two years')
+else:
+    print('you are a kid')
+
+# average length of american people name is 12
+# check the input now
+
+first_name = input(' Enter your first name')
+last_name = input(' Enter your last name')
+full_name = first_name+' '+last_name
+name_length = len(full_name)
+
+if(name_length == 12):
+    print(f'{first_name} {last_name} is good as average length of american people name')
+elif(name_length < 12):
+    print(f'{first_name} {last_name} is shorter then average length of american people name')
+elif(name_length > 12):
+    print(f'{first_name} {last_name} is larger then average length of american people name')
+
