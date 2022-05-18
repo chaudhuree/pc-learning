0dd3de383f5458bf7942e0c2cecb996d585e568c
chaudhuree <chaudhuree00@gmail.com>
2022-05-18 13:13:12 +0600

    docs: range some more

--- Day 05/range.py
+++ Day 05/range.py
@@ -6,3 +6,11 @@ for i in range(1, 5):
 
 for i in range(5):
     print("chaudhuree")
+
+
+# range with step
+for i in range(1, 10, 2):
+    print(i)
+# for desending order must mention the step with negative numbers
+for i in range(10, 1, -2):
+    print(i)
