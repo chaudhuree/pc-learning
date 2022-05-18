bf613eaa22fd2ca3084180c272713854a7dc9a6c
chaudhuree <chaudhuree00@gmail.com>
2022-05-18 12:41:22 +0600

    docs:while pyramid

--- Day 05/loop.py
+++ Day 05/loop.py
@@ -1,5 +1,19 @@
 # while loop
+from itertools import count
+
+
 answer=input('say hi: ')
 while answer != 'hi':
     answer=input('rude,say hi: ')
 print('hi to you too..')
+
+# pyramid using while loop
+count=8
+while count > 0:
+    print(' '*(count-1),'*'*(count))
+    count-=1
+    
+count=1
+while count < 8:
+    print(' '*(count-1),'*'*(count))
+    count+=1
