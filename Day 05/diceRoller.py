52cab4ec37dacfc73abfd1e56b762abb1ab7b9e3
chaudhuree <chaudhuree00@gmail.com>
2022-05-18 17:21:11 +0600

    docs: dice roll

---
+++ Day 05/diceRoller.py
@@ -0,0 +1,17 @@
+import random
+
+num_dice = int(input("How many dice would you like to roll? "))
+num_sides = int(input("How many sides are on each die? "))
+
+while True:
+    result = '|'
+    for i in range(num_dice):
+
+        value = random.randint(1, num_sides)
+        result += f'{value}|'
+        # print(value)
+    print(result)
+
+    quit = input("press q to quit..")
+    if quit == 'q':
+        break
