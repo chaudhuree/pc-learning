212e71b0b3b522ae5398562f538fe3cf87563302
chaudhuree <chaudhuree00@gmail.com>
2022-05-21 22:15:18 +0600

    docs: append and extend method of list append insert element at the end extend add element at the end with destrcture the itterable

--- Day 08/list.py
+++ Day 08/list.py
@@ -9,3 +9,14 @@ print(alist[1])
 # updating list element
 alist[0]=22
 print(alist)
+
+# appending elements to list
+wishlist=['apple','banana','mango']
+print(wishlist)
+wishlist.append('orange')
+
+# append takes itterable object
+fruits=['cherry','watermelon','grapes']
+
+wishlist.extend(fruits)
+print(wishlist)
