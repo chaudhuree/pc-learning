fc4cb3226e509c859b7966acceff42c841ee6dfa
chaudhuree <chaudhuree00@gmail.com>
2022-05-24 23:46:41 +0600

    docs: adidng and updating data to dictionary

--- Day 11/dictionary.py
+++ Day 11/dictionary.py
@@ -2,3 +2,8 @@
 my_dict={'name':'sOhan','age':'27','salary':'50000'}
 # accessing dictionary
 print(my_dict['name'])
+# updating data
+my_dict['name']='chaudhuree'
+# adding data
+my_dict['address']='Panchbibi'
+print(my_dict)
