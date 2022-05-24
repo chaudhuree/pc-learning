9e2810219dc0144e3b61ba72f3990f1744f144ec
chaudhuree <chaudhuree00@gmail.com>
2022-05-24 23:51:30 +0600

    docs: dictionary get method  to find availability of a key

--- Day 11/dictionary.py
+++ Day 11/dictionary.py
@@ -7,3 +7,13 @@ my_dict['name']='chaudhuree'
 # adding data
 my_dict['address']='Panchbibi'
 print(my_dict)
+
+# check if a key is available in dictionary or notScoped
+# in operator
+print('name' in my_dict)
+print('hobby' in my_dict)
+
+# another methot. it will also return the value if available or return none
+# get method
+print(my_dict.get('name'))
+print(my_dict.get('hobby'))
