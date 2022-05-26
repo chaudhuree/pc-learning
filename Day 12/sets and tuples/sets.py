7e1651d5e9e339c7785148d6f148c8c30cb300cc
chaudhuree <chaudhuree00@gmail.com>
2022-05-27 03:57:34 +0600

    docs: sets

---
+++ Day 12/sets and tuples/sets.py
@@ -0,0 +1,93 @@
+# Different types of sets in Python
+# set of integers
+my_set = {1, 2, 3}
+print(my_set)
+
+# set of mixed datatypes
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+# set cannot have duplicates
+# Output: {1, 2, 3, 4}
+# my_set = {1, 2, 3, 4, 3, 2}
+# print(my_set)
+
+# we can make set from a list
+# Output: {1, 2, 3}
+my_set = set([1, 2, 3, 2])
+print(my_set)
+
+# set cannot have mutable items
+# here [3, 4] is a mutable list
+# this will cause an error.
+
+# my_set = {1, 2, [3, 4]}
+
+# initialize my_set
+my_set = {1, 3}
+print(my_set)
+
+# my_set[0]
+# if you uncomment the above line
+# you will get an error
+# TypeError: 'set' object does not support indexing
+
+# add an element
+# Output: {1, 2, 3}
+my_set.add(2)
+print(my_set)
+
+# add multiple elements
+# Output: {1, 2, 3, 4}
+my_set.update([2, 3, 4])
+print(my_set)
+
+# add list and set
+# Output: {1, 2, 3, 4, 5, 6, 8}
+my_set.update([4, 5], {1, 6, 8})
+print(my_set)
+
+# Difference between discard() and remove()
+
+# initialize my_set
+my_set = {1, 3, 4, 5, 6}
+print(my_set)
+
+# discard an element
+# Output: {1, 3, 5, 6}
+my_set.discard(4)
+print(my_set)
+
+# remove an element
+# Output: {1, 3, 5}
+my_set.remove(6)
+print(my_set)
+
+# discard an element
+# not present in my_set
+# Output: {1, 3, 5}
+my_set.discard(2)
+print(my_set)
+
+# remove an element
+# not present in my_set
+# you will get an error.
+# Output: KeyError
+
+# my_set.remove(2)
+
+
+# in keyword in a set
+# initialize my_set
+my_set = set("apple")
+
+# check if 'a' is present
+# Output: True
+print('a' in my_set)
+
+# check if 'p' is present
+# Output: False
+print('p' not in my_set)
+
+for letter in set("apple"):
+    print(letter)
