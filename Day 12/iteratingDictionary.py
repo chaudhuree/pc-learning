5b1254e10176325a3ee97cf65e66dbd4c7a72237
chaudhuree <chaudhuree00@gmail.com>
2022-05-27 00:35:13 +0600

    dictionary iteration. key values and items

---
+++ Day 12/iteratingDictionary.py
@@ -0,0 +1,17 @@
+test_score={
+    'shahriar':87,
+    'kabir':80,
+    'sOhan':95,
+    'chaudhuree':83,
+    'kazi':78,
+    'sabrina':90
+}
+for key,value in test_score.items():
+    print(key,value)
+print("\n")
+for key in test_score.keys():
+    print(key) 
+print("\n")
+for value in test_score.values():
+    print(value)
+print("\n")
