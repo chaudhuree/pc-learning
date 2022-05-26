54cb98c307b30fd6db1c6ba35c73a64ba8afb2c3
chaudhuree <chaudhuree00@gmail.com>
2022-05-27 01:26:45 +0600

    docs: dictionary union

--- Day 12/updateDictionary.py
+++ Day 12/updateDictionary.py
@@ -1,10 +1,19 @@
-order={'cost':34,'quantity':2}
+order = {'cost': 34, 'quantity': 2}
 
-order.update({'name':'tacho','date':"23/04/2022"})
+order.update({'name': 'tacho', 'date': "23/04/2022"})
 print(order)
+print("\n")
 
 # combne dictionary
-dict_1={'name':'tacho','date':"23/04/2022"}
-dict_2={'cost':34,'quantity':2}
-main_dict={**dict_1,**dict_2}
+print('combine dictionary')
+dict_1 = {'name': 'tacho', 'date': "23/04/2022"}
+dict_2 = {'cost': 34, 'quantity': 2}
+main_dict = {**dict_1, **dict_2}
 print(main_dict)
+print("\n")
+
+# union
+print('union')
+union_dict = dict_2 | dict_1
+print(union_dict)
+print("\n")
