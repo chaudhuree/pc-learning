3125daaccfc28be697d65339ffeaafc939c6e1c2
chaudhuree <chaudhuree00@gmail.com>
2022-05-27 01:37:03 +0600

    docs: dictiionary and list

---
+++ Day 12/dictionary_list.py
@@ -0,0 +1,11 @@
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+print(people[1]['name'])
+print(people[1]['age'])
+print(people[1]['sex'])
+
+student_info = {'Student_id':[85, 47, 49], 'Student_age':[91, 116, 913]}
+
+print("values from list1:",student_info['Student_id'][0])
+print("values from second list:",student_info['Student_age'])
