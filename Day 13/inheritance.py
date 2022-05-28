b3618f75123328185e95ff74d1b36de390291e48
chaudhuree <chaudhuree00@gmail.com>
2022-05-28 22:44:21 +0600

    docs: super()

--- Day 13/inheritance.py
+++ Day 13/inheritance.py
@@ -1,5 +1,6 @@
 class Cat:
     def __init__(self, name):
+        print('inside Cat init')
         self.name = name
 
     def meow(self):
@@ -11,7 +12,11 @@ class Cougor(Cat):
         print(f"{self.name} Purrs!")
 
 
-class Lion(Cougor):
+class Lion(Cat):
+    def __init__(self, name):
+        print("inside Lion init")
+        super().__init__(name)
+
     def roar(self):
         print(f"{self.name} roars!")
 
