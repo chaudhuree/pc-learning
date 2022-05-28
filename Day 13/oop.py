e6fae5862515193962579ab41bac487977729755
chaudhuree <chaudhuree00@gmail.com>
2022-05-28 21:09:50 +0600

    docs: class methods

--- Day 13/oop.py
+++ Day 13/oop.py
@@ -8,6 +8,19 @@ class Dog:
     def bark(self):
         print(f"{self.name} says WOOF!")
 
+    def learn_trick(self, new_trick):
+        if new_trick not in self.tricks:
+            self.tricks.append(new_trick)
+
+    def perform_trick(self, trick):
+        if trick in self.tricks:
+            print(f"{self.name} performs {trick}")
+        else:
+            print(f"{self.name} does not know {trick}")
+
 
 myDog = Dog("Fido", "Mixed", "Indon")
 myDog.bark()
+myDog.learn_trick("roll over")
+myDog.perform_trick("roll over")
+myDog.perform_trick("sit")
