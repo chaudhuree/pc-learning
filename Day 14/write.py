0335a71572abe41da1ed8c780fad824f4a40ff57
chaudhuree <chaudhuree00@gmail.com>
2022-05-29 14:43:10 +0600

    docs: more method of i/o

--- Day 14/write.py
+++ Day 14/write.py
@@ -1,2 +1,11 @@
 with open("D:\codes\python\pc-learning\Day 14\write.txt", "w") as f:
     f.write("chaudhuree \n"*6)
+# To write to a file, first open it in "w" mode
+with open("./Day 14/haiku.txt", "w") as file:
+    file.write("Here's one more haiku\n")
+    file.write("What about the older one?\n")
+    file.write("Let's go check it out")
+
+# You can also write to files that don't yet exist
+with open("./Day 14/lol.txt", "w") as file:
+    file.write("haha" * 10)
