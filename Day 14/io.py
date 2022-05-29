a451b222018e3ed02896b94548bce21df7b912a0
chaudhuree <chaudhuree00@gmail.com>
2022-05-29 14:07:08 +0600

    docs:basic file read operations

--- Day 14/io.py
+++ Day 14/io.py
@@ -2,5 +2,14 @@
 
 f= open("D:\codes\python\pc-learning\Day 14\story.txt")
 print(f.read()) # read the file
-f.seek(4) # reset the cursor to the beginning of the file
+print('\n')
+f.seek(4) # reset the cursor to the second word of the file
 print(f.read()) # read the file
+print('\n')
+f.seek(0) # reset the cursor to the beginning of the file
+print(f.readline()) # read the first line of the file
+f.seek(0) # reset the cursor to the beginning of the file
+print('\n')
+print(f.closed) # check if the file is closed
+f.close() # close the file
+print(f.closed) # check if the file is closed
