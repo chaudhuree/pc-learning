3c734aa2ee123953895469c917d580a8f3076d15
chaudhuree <chaudhuree00@gmail.com>
2022-05-29 14:30:18 +0600

    docs: write

--- Day 14/io2nd.py
+++ Day 14/io2nd.py
@@ -4,3 +4,4 @@ with open('D:\codes\python\pc-learning\Day 14\story.txt') as f:
 
 print(f.closed)
 print(data)
+# with block closes the file automatically
---
+++ Day 14/write.py
@@ -0,0 +1,2 @@
+with open("D:\codes\python\pc-learning\Day 14\write.txt", "w") as f:
+    f.write("chaudhuree \n"*6)
